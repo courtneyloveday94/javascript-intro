eb4c1a4885c2a742455a8b1b0c74ddaf35b834be
courtney sarah loveday <845862270@kent-202-12m-2.local>
2016-04-13 10:01:37 +1200

    start validation code

--- js/validation.js
+++ js/validation.js
@@ -3,6 +3,9 @@
 var registrationForm = document.querySelector('#registration')
 
 // wait for the user to submit the form
-registrationForm.onsubmit = function() {
-	alert('submitted');
+registrationForm.onsubmit = function(event) {
+
+	// stop the form from submiting
+	event.preventDefault();
+
 }
