8f76cb69299df901589eb377dbdaf485714da8b4
courtney sarah loveday <845862270@kent-202-12m-2.local>
2016-04-15 09:55:40 +1200

    added campus' and patterns.js file

--- js/validation.js
+++ js/validation.js
@@ -1,19 +1,21 @@
 // find the form
-var registrationForm = document.querySelector('#registration')
+var registrationForm = document.querySelector('#registration');
 
 // wait for the user to submit the form
 registrationForm.onsubmit = function(event) {
 
-	// patterns to use in validation
-	var usernamePattern = new RegExp("^[a-zA-Z0-9._-]{3,20}$");
-	var namePattern = new RegExp("^[a-zA-Z '.-]{1,30}$")
+	// count how many errors there are
+	var totalErrors = 0;
 
 	// get references to the input fields
 	var usernameInput = document.querySelector("#username");
-	var usernameMessage = document.querySelector("#username-message")
+	var usernameMessage = document.querySelector("#username-message");
 
-	var nameInput = document.querySelector("#full-name")
-	var nameMessage = document.querySelector("#full-name-message")
+	var nameInput = document.querySelector("#full-name");
+	var nameMessage = document.querySelector("#full-name-message");
+
+	var campusMessage = document.querySelector("#campus-message");
+	var campusOptions = document.querySelectorAll("[name=campus]");
 
 	// check the username
 	if(usernamePattern.test(usernameInput.value)) {
@@ -21,16 +23,39 @@ registrationForm.onsubmit = function(event) {
 		usernameMessage.innerHTML = "";
 	} else {
 		// username is invalid
-		usernameMessage.innerHTML = "Username is invalid. Usernames must only contain the characters 0-9 A-Z a-z ; . -  with a length of 3-15 characters.";
+		usernameMessage.innerHTML = "Username is invalid.";
+		totalErrors++;
 	}
 
 	if(namePattern.test(nameInput.value)) {
 		nameMessage.innerHTML = "";
 	} else {
-		nameMessage.innerHTML = "Name is invalid. Names must only contain the characters A-Z a-z ; . -";
+		nameMessage.innerHTML = "Name is invalid.";
+		totalErrors++;
 	}
 
-	// stop the form from submiting
-	event.preventDefault();
+	// loop over all the campus options
+	var campusIsSelected = false;
+
+	for(var i=0; i<campusOptions.length; i++) {
+		// check if this campus has been selected
+		if(campusOptions[i].checked) {
+			campusIsSelected = true;
+		}
+	}
+
+	// if campusIsSelected is still false
+	if(campusIsSelected == false) {
+		campusMessage.innerHTML = "Please select a campus.";
+		totalErrors++;
+	} else {
+		campusMessage.innerHTML = "";
+	}
+
+	// if the total errors are greater than 0
+	if(totalErrors > 0) {
+		// stop the form from submiting
+		event.preventDefault();
+	}	
 
 }
